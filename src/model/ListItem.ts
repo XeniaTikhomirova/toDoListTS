0deec34019cbe61e5b0dfaa203e7c834915c30d0
Kseniia <xeniiatikhomirova@gmail.com>
2024-04-26 22:50:28 +0400

    added constructor of thr class ListItem

--- src/model/ListItem.ts
+++ src/model/ListItem.ts
@@ -1,9 +1,37 @@
 export interface Item {
    id: string;
-   discription: string;
-   cheStatus: boolean;
+   item: string;
+   checkStatus: boolean;
 }
-
 export default class ListItem implements Item {
-   
+
+   constructor(
+      private _id: string = "",
+      private _item: string = "",
+      private _checkStatus: boolean = false
+   ) {}
+
+   get id(): string {
+      return this._id;
+   }
+
+   set id(id: string) {
+      this._id = id;
+   }
+
+   get item(): string {
+      return this._item;
+   }
+
+   set item(item: string) {
+      this._item = item;
+   }
+
+   get checkStatus(): boolean {
+      return this._checkStatus;
+   }
+
+   set checkStatus(checkStatus: boolean) {
+      this._checkStatus = checkStatus;
+   }
 }
